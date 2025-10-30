2d66d4949dc03123d76d633cd45aa27569e61f5a
Franco Quinteros <fquinteros701@gmail.com>
2025-10-30 20:24:35 -0300

    se monto la api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import Player from './components/Player'
+import Home from './pages/Home'
 
 function App() {
   
 
   return (
     <>
-      
+    <Home/>
+    <Player/>
     </>
   )
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
 
-function Player() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import React, { useRef, useEffect } from 'react';
+import { useSongs } from '../context/SongsContext'; 
 
-export default Player
+const Player = () => {
+
+    const { currentSong } = useSongs(); 
+    
+
+    const audioRef = useRef(null); 
+    
+
+    const audioUrl = currentSong ? currentSong.url_cancion : '';
+
+
+    useEffect(() => {
+        if (audioUrl && audioRef.current) {
+   
+            audioRef.current.load();
+            
+
+            audioRef.current.play().catch(error => {
+
+                console.warn("Reproducción automática bloqueada. El usuario debe interactuar con el reproductor.", error);
+            });
+        }
+    }, [audioUrl]); 
+
+    return (
+        <div className="music-player-container">
+             <audio 
+                ref={audioRef} 
+                id="music-player" 
+                src={audioUrl} 
+                controls 
+                preload="auto" 
+            />
+            {currentSong && <p>Reproduciendo: {currentSong.titulo} - {currentSong.artista}</p>}
+        </div>
+    );
+};
+
+export default Player;
