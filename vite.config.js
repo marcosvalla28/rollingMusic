0b2740081b90b0ac554af4080f128d7065bafac9
LUISS005 <luisandoval.3232@gmail.com>
2025-11-01 10:05:09 -0300

    agrego configuracion necesaria en vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.deezer.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
 
