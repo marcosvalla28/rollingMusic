900fceeb115c4b0687c5ab6f43ae72641c5b2bb5
LUISS005 <luisandoval.3232@gmail.com>
2025-11-01 08:42:21 -0300

    cambio array de canciones para poder usar la API

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,37 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 
-const cancionCard = () => {
-  const secciones = [{
-      titulo: "Recomendadas",
-      canciones: [
-        { imagen: "https://picsum.photos/200", titulo: "Blinding Lights", artista: "The Weeknd" },
-        { imagen: "https://picsum.photos/200", titulo: "Levitating", artista: "Dua Lipa" },
-        { imagen: "https://picsum.photos/200", titulo: "Peaches", artista: "Justin Bieber" },
-        { imagen: "https://picsum.photos/200", titulo: "Good 4 U", artista: "Olivia Rodrigo" },
-        { imagen: "https://picsum.photos/200", titulo: "Montero", artista: "Lil Nas X" },
-        { imagen: "https://picsum.photos/200", titulo: "Stay", artista: "The Kid LAROI" },
-      ],},
-    {
-      titulo: "Lo más escuchado",
-      canciones: [
-        { imagen: "https://picsum.photos/200", titulo: "Heat Waves", artista: "Glass Animals" },
-        { imagen: "https://picsum.photos/200", titulo: "Industry Baby", artista: "Lil Nas X" },
-        { imagen: "https://picsum.photos/200", titulo: "Shivers", artista: "Ed Sheeran" },
-        { imagen: "https://picsum.photos/200", titulo: "Bad Habits", artista: "Ed Sheeran" },
-        { imagen: "https://picsum.photos/200", titulo: "Butter", artista: "BTS" },
-        { imagen: "https://picsum.photos/200", titulo: "Save Your Tears", artista: "The Weeknd" },
-      ],},
-    {
-      titulo: "Nuevos lanzamientos",
-      canciones: [
-        { imagen: "https://picsum.photos/200", titulo: "Easy On Me", artista: "Adele" },
-        { imagen: "https://picsum.photos/200", titulo: "Cold Heart", artista: "Elton John & Dua Lipa" },
-        { imagen: "https://picsum.photos/200", titulo: "Smokin Out The Window", artista: "Bruno Mars" },
-        { imagen: "https://picsum.photos/200", titulo: "Message In A Bottle", artista: "Taylor Swift" },
-        { imagen: "https://picsum.photos/200", titulo: "Moth To A Flame", artista: "Swedish House Mafia" },
-        { imagen: "https://picsum.photos/200", titulo: "One Right Now", artista: "Post Malone" },
-      ],},
-  ];
+
+const Canciones = () => {     //define estado inical de las secciones 
+  const [secciones, setSecciones] = useState([
+    { titulo: "Recomendadas", data: [], loading: true, error: null },
+    { titulo: "Lo más escuchado", data: [], loading: true, error: null },
+    { titulo: "Nuevos lanzamientos", data: [], loading: true, error: null },
+    { titulo: "Top globales", data:[], loading: true, error: null},
+  ]);
+
 
   const Cards = ({ titulo, canciones }) => (
     <div className="mb-12">
