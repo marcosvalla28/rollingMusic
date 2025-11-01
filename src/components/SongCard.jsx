1375317dc06cbbe7a63c53391c8585a1f3c992db
LUISS005 <luisandoval.3232@gmail.com>
2025-11-01 08:44:58 -0300

    agrego funcion para traer las canciones

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -9,49 +9,20 @@ const Canciones = () => {     //define estado inical de las secciones
     { titulo: "Top globales", data:[], loading: true, error: null},
   ]);
 
+  const fetchCanciones = async (index, endpoint) => {     //funcion para traer las canciones
+    try {
 
-  const Cards = ({ titulo, canciones }) => (
-    <div className="mb-12">
-      <h2 className="text-4xl font-bold text-white mb-6">{titulo}</h2>
-      <div className="flex overflow-x-auto space-x-4 pb-2 scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-black">
-        {canciones.map((cancion, index) => (
-          <div
-            key={index}
-            className="shrink-0 w-44 bg-neutral-900 rounded-lg p-3 text-center text-white hover:scale-105 transition-transform duration-300 cursor-pointer">
-            {cancion.imagen ? (
-              <img
-                src={cancion.imagen}
-                alt={cancion.titulo}
-                className="w-full h-44 object-cover rounded-md mb-2"
-                loading="lazy"
-              />
-            ) : (
-              <div className="bg-gray-700 border-2 border-dashed border-gray-600 rounded-md w-full h-44 mb-2 flex items-center justify-center">
-                <span className="text-gray-500 text-xs">Sin imagen</span>
-              </div>
-            )}
+      const response = await fetch(endpoint);     //peticion http a la url
+      if (!response.ok) throw new Error(` Error en HTTP ${response.status}`);
+      const result = await response.json();  
+      const tracks = result.data || [];     
+      
+    } catch (error) {
+      console.error(`Error en sección ${index}:`, error);            
+    }
+  };
 
-            <h4 className="text-lg font-semibold truncate">{cancion.titulo}</h4>
-            <p className="text-sm text-gray-400 truncate">{cancion.artista}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-
-  return (
-    <div className="bg-black min-h-screen p-6">
-      <div className="max-w-6xl mx-auto">
-        {secciones.map((seccion, index) => (
-          <Cards
-            key={index}
-            titulo={seccion.titulo}
-            canciones={seccion.canciones}
-          />
-        ))}
-      </div>
-    </div>
-  );
+  
 };
 
 export default cancionCard
