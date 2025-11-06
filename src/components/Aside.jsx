9054b7afbb94af15471e153218a15177d490fe0e
Juanmd14 <juanmd149@gmail.com>
2025-11-06 01:09:14 -0300

    cambios en navbar,aside y menus

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,14 +1,14 @@
 import SideMenuItem from "./SideMenuItem";
 
-function Aside() {
+function Aside({ onItemClick }) {
   return (
     <nav className="flex flex-col flex-1 bg-linear-to-b from-purple-950/40 to-black/40 rounded-lg p-2 text-white">
       <ul className="flex flex-col space-y-2">
-        <SideMenuItem href="/">Inicio</SideMenuItem>
-        <SideMenuItem href="/artista">Artista</SideMenuItem>
-        <SideMenuItem href="/album">Álbum</SideMenuItem>
-        <SideMenuItem href="/nosotros">Sobre Nosotros</SideMenuItem>
-        <SideMenuItem href="/playlist">Lista de Reproducción</SideMenuItem>
+        <SideMenuItem href="/" onClick={onItemClick}>Inicio</SideMenuItem>
+        <SideMenuItem href="/artista" onClick={onItemClick}>Artista</SideMenuItem>
+        <SideMenuItem href="/album" onClick={onItemClick}>Álbum</SideMenuItem>
+        <SideMenuItem href="/nosotros" onClick={onItemClick}>Sobre Nosotros</SideMenuItem>
+        <SideMenuItem href="/playlist" onClick={onItemClick}>Lista de Reproducción</SideMenuItem>
       </ul>
     </nav>
   );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,6 @@ import { useSongs } from '../context/SongsContext';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMusic } from "@fortawesome/free-solid-svg-icons";
 
-
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const { handleSearch } = useSongs();
@@ -16,20 +15,25 @@ const SearchBar = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="search-bar">
-      <input className='searchbox'
+    <form onSubmit={handleSubmit} className="flex items-center gap-2">
+      <input 
+        className="rounded-2xl bg-purple-900/30 w-[500px] h-[35px] text-center mr-4
+                   hover:bg-purple-700/30 focus:outline-none focus:ring-2 focus:ring-purple-500
+                   lg:w-[500px] md:w-[250px] max-sm:w-[200px]
+                   text-white placeholder-gray-400"
         type="text"
         placeholder="Buscar canción, artista o grupo..."
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
 
-      <div>
-        <button type="submit" className='nota'>
-          <FontAwesomeIcon icon={faMusic} size='xl' />
+      <button 
+        type="submit" 
+        className="w-10 h-10 flex items-center justify-center bg-[#691175] rounded-full
+                   hover:shadow-[0_0_10px_#ffffff] transition-shadow"
+      >
+        <FontAwesomeIcon icon={faMusic} size='xl' />
       </button>
-      </div>
-
     </form>
   );
 };
