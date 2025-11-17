965cbbc27ffaeedbd37be71510250c922bf1edfd
Franco Quinteros <fquinteros701@gmail.com>
2025-11-17 01:20:40 -0300

    se corrigio el package.json

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/deezer': {
-        target: 'https://api.deezer.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/deezer/, ''),
-      },
-    },
-  },
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
@@ -23,4 +14,3 @@ export default defineConfig({
     },
   },
 })
-
