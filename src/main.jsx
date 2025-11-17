55188fe74277f3bb76375e006ab7c4ddd8ff05b5
Franco Quinteros <fquinteros701@gmail.com>
2025-11-17 00:33:47 -0300

    correcion en un import de main.jsx y en env.example

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import AppRouter from './routes/AppRouter';
 import { SongsProvider } from './context/SongsContext'; 
 import { AuthProvider } from './context/AuthContext';
-import './app.css';
+import './App.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
